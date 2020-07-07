c769504fe8d60907364366c9513f92cbb2d763f4
rohezal <rashid@milacom.de>
2020-07-07 10:47:14 +0200

    added argument for file to use by converter

--- converter.py
+++ converter.py
@@ -7,11 +7,20 @@ Created on Wed Mar 25 00:06:57 2020
 """
 
 import csv
+import sys
 
-outfile = open('converted_BRICS.csv', 'w')
+number_of_arguments = len(sys.argv)
+if(number_of_arguments == 2):
+	filename = sys.argv[1]
+	
+else:
+	print("usage: converter.py name_of_the_file.csv. Using now converted_BRICS.csv as a default file")
+	filename = 'converted_BRICS.csv'	
+
+outfile = open('converted_'+filename, 'w')
 csvwriter = csv.writer(outfile )
 
-with open('BRICS.csv') as csv_file_brics:    
+with open(filename) as csv_file_brics:    
     
     csv_reader_brics = csv.reader(csv_file_brics, delimiter=',')
     for row in csv_reader_brics:
